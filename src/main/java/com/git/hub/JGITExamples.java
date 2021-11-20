b9b1ed58f0ea6a1b9ac2affd6b23558bf87d74f3
shaid <shaid@shaids-MacBook-Pro.local>
2021-11-20 07:29:55 +0530

    refector

--- src/main/java/com/git/hub/JGITExamples.java
+++ src/main/java/com/git/hub/JGITExamples.java
@@ -1,6 +1,5 @@
 package com.git.hub;
 
-import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -9,17 +8,16 @@ public class JGITExamples {
 
     public static void main(String[] args) throws Exception {
 
+        /* Get Json changes from git */
         Path path = Paths.get("/Users/shaid/Documents/MyWorld");
         String cmd = "git -C /Users/shaid/Documents/MyWorld/ diff d3b2ca47f10eb97116c0ca02247dd885f72ae071";
-
         List<Diff> gitChanges = Git.getGitChanges(path, cmd);
 
+        /* Get DB scripts corresponding to json changes */
         DBScript dbScript = new DBScript(gitChanges);
         String fileName = "dbscript";
-
         List<String> dbScripts = dbScript.getDBScripts(dbScript.getFileAsIOStream(fileName));
-        System.out.println("db scripts. to be run..");
-        System.out.println(dbScripts);
+        System.out.println("db scripts. to be run.." + dbScripts);
 
     }
 }
